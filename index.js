88a340db6de9304964af344466c0ab4b2592cd6b
Najma <najma.a16@gmail.com>
2022-11-01 15:39:12 -0500

    commit tree 08e7a5b0649ea337d9fea09652c9618d02f916f0

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(defaultactivity = "roller-skate"){
+   return `This Saturday, I want to ${defaultactivity}!`
+}
+function mondayWork(defaultactivity = "go to the office"){
+    return `This Monday, I will ${defaultactivity}.`
+}
+function wrapAdjective(adjective = "special"){
+
+    wrapAdjective("%")("a dedicated programmer");
+    
+}
